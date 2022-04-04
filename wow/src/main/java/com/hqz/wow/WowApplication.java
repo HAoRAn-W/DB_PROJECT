f66e9d288851e2dcc5285dea2a793245f2e5889e
HAoRAn-W <haoran2326@gmail.com>
2022-04-04 16:11:08 -0400

    Add SpringBoot project

---
+++ wow/src/main/java/com/hqz/wow/WowApplication.java
@@ -0,0 +1,13 @@
+package com.hqz.wow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WowApplication.class, args);
+    }
+
+}
---
+++ wow/src/test/java/com/hqz/wow/WowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hqz.wow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
