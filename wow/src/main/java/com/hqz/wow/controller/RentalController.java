d38646dda820635820b9b1dece05b82f74c07888
HAoRAn-W <haoran2326@gmail.com>
2022-04-14 11:03:52 -0400

    Add viewcar page, modified the url

--- wow/src/main/java/com/hqz/wow/controller/RentalController.java
+++ wow/src/main/java/com/hqz/wow/controller/RentalController.java
@@ -4,6 +4,7 @@ import com.hqz.wow.entity.ClassEntity;
 import com.hqz.wow.service.ClassService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -23,11 +24,11 @@ public class RentalController {
         return "index";
     }
 
-    @RequestMapping("/carclass/{classId}")
+    @RequestMapping("/view-car/{classId}")
     public String carClass(@PathVariable int classId, Model model) {
         // todo
         ClassEntity classEntity = classService.getClassEntityById(classId);
         model.addAttribute("carClass", classEntity);
-        return "carclass";
+        return "viewcar";
     }
 }
