dc4c0285755a4d2f719ba6165d889b3f05087509
HAoRAn-W <haoran2326@gmail.com>
2022-04-20 18:34:04 -0400

    Add AdminSecurityConfig for Admin login configurations

---
+++ wow/src/main/java/com/hqz/wow/config/AdminSecurityConfig.java
@@ -0,0 +1,6 @@
+package com.hqz.wow.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class AdminSecurityConfig extends WebSecurityConfigurerAdapter {
+}
