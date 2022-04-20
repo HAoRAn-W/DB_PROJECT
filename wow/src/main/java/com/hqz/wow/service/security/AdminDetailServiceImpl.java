3195801e2183d6a9532e7dbb3f3723cb2a59e852
HAoRAn-W <haoran2326@gmail.com>
2022-04-20 18:34:39 -0400

    Add AdminDetailServiceImpl to implement admin login

---
+++ wow/src/main/java/com/hqz/wow/service/security/AdminDetailServiceImpl.java
@@ -0,0 +1,12 @@
+package com.hqz.wow.service.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AdminDetailServiceImpl implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
