49b671a2fc117dbfae2ad6fe146dc426f091061c
HAoRAn-W <haoran2326@gmail.com>
2022-05-04 21:09:41 -0400

    add csrf token post

--- wow/src/main/java/com/hqz/wow/config/AdminSecurityConfig.java
+++
@@ -1,6 +0,0 @@
-package com.hqz.wow.config;
-
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-public class AdminSecurityConfig extends WebSecurityConfigurerAdapter {
-}
