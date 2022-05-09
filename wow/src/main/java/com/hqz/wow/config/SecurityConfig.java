ae72595b211bc5fc94badf2f6ba83be787c2a435
HAoRAn-W <haoran2326@gmail.com>
2022-05-08 21:28:13 -0400

    fixed bugs, add presentation

--- wow/src/main/java/com/hqz/wow/config/SecurityConfig.java
+++ wow/src/main/java/com/hqz/wow/config/SecurityConfig.java
@@ -45,7 +45,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .rememberMe()
                 .userDetailsService(this.userDetailsService)
                 .tokenValiditySeconds(86400)
-                .useSecureCookie(true)
                 .rememberMeCookieName("wow-cookie")
                 //Login configurations
                 .and()
